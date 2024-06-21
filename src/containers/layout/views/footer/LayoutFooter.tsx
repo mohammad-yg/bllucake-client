04b1994ae1e99add0d1984cb1af891b9a9723057
mohammad-yg <mohammadyadegari032@gmail.com>
2024-06-21 05:51:11 +0330

    feat: footer

--- src/containers/layout/views/footer/LayoutFooter.tsx
+++ src/containers/layout/views/footer/LayoutFooter.tsx
@@ -1,13 +1,27 @@
 import { Settings } from '@/apis'
 import React, { FC } from 'react'
+import Link from 'next/link'
+import { LayoutFooterInstagramIcon } from './views/LayoutFooterInstagramIcon'
+import { LayoutFooterTelegramIcon } from './views/LayoutFooterTelegramIcon'
+import { LayoutFooterWhatsAppIcon } from './views/LayoutFooterWhatsAppIcon'
+
 
 type Props = {
     appSettings: Settings,
-    
 }
 
-export const LayoutFooter: FC<Props> = () => {
+export const LayoutFooter: FC<Props> = ({ appSettings }) => {
     return (
-        <div></div>
+        <div className={'h-[86px] w-full bg-[#154160] rounded-tl-xl flex items-center justify-end px-[32px] [&>a]:ms-[24px]'}>
+            <Link href={appSettings.instagram}>
+                <LayoutFooterInstagramIcon />
+            </Link>
+            <Link href={appSettings.telegram}>
+                <LayoutFooterTelegramIcon />
+            </Link>
+            <Link href={appSettings.whatsapp}>
+                <LayoutFooterWhatsAppIcon />
+            </Link>
+        </div>
     )
 }
